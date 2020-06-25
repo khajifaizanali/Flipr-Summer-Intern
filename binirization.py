ce4f7e28a8813221d7544902c3e0e4705d5bec79
KHAJI FAIZAN ALI <39943248+khajifaizanali@users.noreply.github.com>
2020-06-25 17:15:08 +0530

    Update binirization.py

--- binirization.py
+++ binirization.py
@@ -21,7 +21,45 @@ th3 = cv2.bilateralFilter(th3,9,150,75)
 blur = cv2.GaussianBlur(th3,(3,3),0)
 cv2.imshow("hello",th3)
 cv2.waitKey(0)
+def aadharNumVerify(adharNum: str) -> bool:
+    """
+    Takes a N digit aadhar number and
+    returns a boolean value whether that is Correct or Not
+    """
+    verhoeff_table_d = (
+        (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+        (1, 2, 3, 4, 0, 6, 7, 8, 9, 5),
+        (2, 3, 4, 0, 1, 7, 8, 9, 5, 6),
+        (3, 4, 0, 1, 2, 8, 9, 5, 6, 7),
+        (4, 0, 1, 2, 3, 9, 5, 6, 7, 8),
+        (5, 9, 8, 7, 6, 0, 4, 3, 2, 1),
+        (6, 5, 9, 8, 7, 1, 0, 4, 3, 2),
+        (7, 6, 5, 9, 8, 2, 1, 0, 4, 3),
+        (8, 7, 6, 5, 9, 3, 2, 1, 0, 4),
+        (9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
 
+    verhoeff_table_p = (
+        (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+        (1, 5, 7, 6, 2, 8, 3, 0, 9, 4),
+        (5, 8, 0, 3, 7, 9, 6, 1, 4, 2),
+        (8, 9, 1, 6, 0, 4, 3, 5, 2, 7),
+        (9, 4, 5, 3, 1, 2, 6, 8, 7, 0),
+        (4, 2, 8, 6, 5, 7, 3, 9, 0, 1),
+        (2, 7, 9, 3, 8, 0, 6, 4, 1, 5),
+        (7, 0, 4, 6, 9, 1, 3, 2, 5, 8))
+
+    # verhoeff_table_inv = (0, 4, 3, 2, 1, 5, 6, 7, 8, 9)
+
+    def checksum(s: str) -> int:
+        """For a given number generates a Verhoeff digit and
+        returns number + digit"""
+        c = 0
+        for i, item in enumerate(reversed(s)):
+            c = verhoeff_table_d[c][verhoeff_table_p[i % 8][int(item)]]
+        return c
+
+    # Validate Verhoeff checksum
+    return checksum(adharNum) == 0 and len(adharNum) == 12
 def extract_text(th3):
     img1 = th3
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
@@ -66,7 +104,7 @@ def extract_text(th3):
             allAdhaarwords.append(adhaarwords)
     #print adhaar number
     print("-------adhaar number--------")
-    adhaar_num=['','']
+    adhaar_num=[]
     k=0
     for adhaarword in allAdhaarwords:
         print(adhaarword)
@@ -74,6 +112,7 @@ def extract_text(th3):
         x = int(adhaarword[0][6])
         y = int(adhaarword[0][7])
         h = int(adhaarword[0][9])
+        adhaar_num.append('')
         for i in adhaarword:
             adhaar_num[k]+=i[-1]
             las=int(i[8])
@@ -81,10 +120,13 @@ def extract_text(th3):
         if k==1:
             widt-=las
         k+=1
-        cv2.rectangle(oimg, (x, y), (x + widt+20, y + h), (0,0,0), cv2.FILLED)
-        resized = cv2.resize(oimg, (img.shape[0]//2 , img.shape[1]//2), interpolation=cv2.INTER_AREA)
-        cv2.imshow('Result', resized)
-        cv2.waitKey(0)
+        if aadharNumVerify(adhaar_num[k]):
+            cv2.rectangle(oimg, (x, y), (x + widt+20, y + h), (0,0,0), cv2.FILLED)
+            resized = cv2.resize(oimg, (img.shape[0]//2 , img.shape[1]//2), interpolation=cv2.INTER_AREA)
+            cv2.imshow('Result', resized)
+            cv2.waitKey(0)
+        else:
+            adhaar_num[k]=''
     print(adhaar_num)
     return adhaar_num
 adhaar1=extract_text(th3)
